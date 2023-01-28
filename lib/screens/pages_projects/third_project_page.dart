450810f52c875901996264ebb23d0a250057f101
dimitri <dig72000@gmail.com>
2023-01-28 10:28:21 +0100

    adding the third project

--- lib/screens/pages_projects/third_project_page.dart
+++ lib/screens/pages_projects/third_project_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ThirdProjectPage extends StatelessWidget {
   const ThirdProjectPage({super.key});
@@ -19,16 +20,38 @@ class ThirdProjectPage extends StatelessWidget {
                 ),
           SizedBox(height: sizeHeigth/10,),
           
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: sizeWidth/15),
-            child: Text("It's on construction. Don't worry, you'll see more soon ;)", 
-              style : TextStyle(fontSize: sizeHeigth/45), 
-              textAlign: TextAlign.justify, 
-            ),
-          ),
+         Container(
+              width: sizeHeigth/3,
+              height: sizeHeigth/4,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(15)),
+                border: Border.all(width: 5, color: Colors.blueGrey)),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                   IconButton(
+                    iconSize: sizeHeigth/20,
+                    onPressed: () async {
+                          final url = Uri(
+                          scheme: 'https',
+                          host: 'github.com',
+                          path: '/KodiakFR/my_mood',
+                        );
+                        if(await canLaunchUrl(url)) {
+                          await launchUrl(url);
+                        } 
+                            },
+                       icon: Image.asset("assets/icons/git.png")),
+                  Padding(
+                    padding: EdgeInsets.only(top: sizeHeigth/65),
+                    child: Text("name : my mood application", textAlign: TextAlign.justify,),
+                  )
 
-        ],
-      ),
-    );
+              ],
+            ),
+          )
+        ]));
   }
 }
